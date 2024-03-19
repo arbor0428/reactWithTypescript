d418bbf52777aa165db50f1c55a0b28c444eae2e
Hannah <hannah1155@naver.com>
2024-03-19 19:21:54 +0900

    context api추가

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Todo from '../modules/Todo';
+import React, { useContext } from 'react';
 import TodoItem from './TodoItem';
+import { TodosContext } from '../store/todos-context';
 
-const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
+const Todos: React.FC = () => {
+    const todoCtx = useContext(TodosContext);
+    
     return (
         <ul>
-            {props.items.map((item)=> (
-                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
+            {todoCtx.items.map((item)=> (
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={todoCtx.removeTodo.bind(null, item.id)} />
             ))}
         </ul>
     )
